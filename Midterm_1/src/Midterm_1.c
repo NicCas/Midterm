631ca11a7ac8d79a3cdcde8ccd498a2c172b7ad7
Nicole C <nicas@sonic.net>
2019-03-12 18:33:16 -0700

    final push

--- Midterm_1/src/Midterm_1.c
+++ Midterm_1/src/Midterm_1.c
@@ -1,17 +1,52 @@
 /*
- ============================================================================
- Name        : Midterm_1.c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
+ * Write a program to print a multiplication table for integers.
+ * The program should input a single number from the user that corresponds to the row and column size.
+ * Additionally, the table should show rows, columns and symbols as shown below.
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+
+	int uInput;
+
+	printf("To generate your multiplication table, enter your desired positive integer here:\n");
+	scanf("%d", &uInput);
+	//printf("You entered: %d\n", uInput);
+
+	int multTable [uInput][uInput];
+
+
+	for (int i = 1; i <= uInput; i++){
+		printf("	%d", i);
+	}
+	printf("\n");
+
+	printf(" +");
+	for (int j = 1; j <= uInput*2; j++){
+		printf("-----");
+	}
+	printf("\n");
+
+
+
+	for (int row = 0; row < uInput; row++){
+		printf("%d|	", row + 1);
+
+			for (int col = 0; col < uInput; col++){
+				int a = row + 1;
+				int b = col + 1;
+
+				multTable [row][col] = a * b;
+
+				printf("%d	", multTable [row][col]);
+
+			}
+			printf ("\n");
+		}
+
 	return EXIT_SUCCESS;
 }
